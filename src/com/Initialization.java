19b21d20dfdd2b3ce5cd282c99bb0541f691bc4f
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-04-17 01:43:25 +0200

    Update Initialization.java

--- src/com/Initialization.java
+++ src/com/Initialization.java
@@ -4,21 +4,27 @@ package com;
 // Initialization class
 public class Initialization {
 
+    // Static class' field
     static int index = callMe(1);
+    // Class' field
     int index1 = callMe(2);
 
+    // Constructor
     public Initialization(){
         callMe(3);
     }
 
+    // Initialization block
     {
         callMe(4);
     }
 
+    // Static block
     static {
         callMe(5);
     }
 
+    // Method to print integer number and return it
     public static int callMe(int number){
         System.out.println(number);
         return number;
